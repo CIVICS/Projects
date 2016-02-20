405e7d80e287379266a5926b7bc4082b57afa273
Dazza Greenwood <dazza@civics.com>
2016-02-20 16:39:17 -0500

    Create Eximchain.js

---
+++ projects/Eximchain.js
@@ -0,0 +1,6 @@
+{"project_name":"Eximchain",
+"project_blurb":"Project Blurb coming soon!",
+"project_url":"URL Coming Soon",
+"project_demo_url":"Will link to YouTube Video",
+"project_source":"MIT/Law Open Summer Skunkworks Project",
+"project_people": "Can,Chris,Jing!"}
